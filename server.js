4dfc29d34378fec84e88700d312de75512030349
Thai <lydangthaihung@gmail.com>
2024-02-28 09:11:50 +0700

    compare hash password

--- server.js
+++ server.js
@@ -9,21 +9,13 @@ const saltRounds = 12;
 /* const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123'; */
 
-
-app.post('/hash', (req, res) => {
-  const plaintextPassword = 'sUperpassw0rd!';
-
-  // Hash the password using BCrypt
-  bcrypt.hash(plaintextPassword, saltRounds, (err, hash) => {
-    if (err) {
-      console.error('Error hashing password:', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      // Send the hashed password to the client or store it in the database
-      res.send('Hashed Password: ' + hash);
-    }
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare('passw0rd!', hash, (err, res) => {
+      console.log(res); //true
+    });
   });
-});
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
