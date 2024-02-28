6ab7f327c839844a5d3b5c95936cf71766d1d7d2
Thai <lydangthaihung@gmail.com>
2024-02-28 09:50:24 +0700

    final

--- server.js
+++ server.js
@@ -14,16 +14,13 @@ app.get('/', (req, res) => {
 });
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
     bcrypt.compare(myPlaintextPassword, hash,(err, res) => {
-        console.log(hash);
-    
     });
 });
 
 var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(result);
 var result = bcrypt.compareSync(myPlaintextPassword, hash);
-
 console.log(result);
 
 //END_ASYNC
